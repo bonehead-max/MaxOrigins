8e392166a3ee9b4c869a68db086044d2104b78d6
bonehead-max <83553947+bonehead-max@users.noreply.github.com>
2021-05-30 19:29:44 -0400

    Update MaxOriginsPowers.java

--- src/java/io/github/bonehead-max/maxorigins/power/MaxOriginsPowers.java
+++ src/java/io/github/bonehead-max/maxorigins/power/MaxOriginsPowers.java
@@ -1,1 +1,11 @@
-// lets goooo
+package io.github.bonehead-max.maxorigins.power;
+
+import io.github.apace100.origins.power.Power;
+import io.github.apace100.origins.power.PowerType;
+import io.github.apace100.origins.power.PowerTypeReference;
+import net.minecraft.util.Identifier;
+
+public class MaxOriginsPowers {
+  public static final PowerType<Power> TASTY = new PowerTypeReference(new Identifier("maxorigins", "tasty"));
+  public static final PowerType<Power> INFECTED = new PowerTypeReference(new Identifier("maxorigins", "infected"));
+}
